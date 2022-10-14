a50f825bef79daec1f8f5a78c198576d0392037e
Akib Sayyad <akisayya@publicisgroupe.net>
2022-10-14 23:27:03 +0530

    Modified

--- spring-unit-testing/src/test/java/com/unittesting/controller/ItemControllerTest.java
+++ spring-unit-testing/src/test/java/com/unittesting/controller/ItemControllerTest.java
@@ -1,6 +1,5 @@
 package com.unittesting.controller;
 
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,31 +7,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@WebMvcTest(ItemController.class)
 @RunWith(SpringRunner.class)
-@WebMvcTest(HelloController.class) // Define Class to Test
-public class HelloControllerTest {
+public class ItemControllerTest {
 
 	@Autowired
-	MockMvc mockMvc;
+	MockMvc mock;
 
 	@Test
-	public void testHello() throws Exception {
-		RequestBuilder request = MockMvcRequestBuilders
-				.get("/hello")
-				.accept("application/json");
+	public void testDummyItem() throws Exception {
+
+
+		RequestBuilder builder = MockMvcRequestBuilders.get("/dummy-item")
+										.accept(MediaType.APPLICATION_JSON);
 		
-		MvcResult result = mockMvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string("Hello"))
-				.andReturn();
-		
-		//assertEquals("Hello", result.getResponse().getContentAsString());
+		mock.perform(builder).andExpect(status().isOk())
+			.andExpect(content().json("{\"id\": 1 ,\"name\":\"Dummy\",\"quantity\":10,\"price\":100.0}"))
+			.andReturn();
+
 	}
 
 }
+
+// content().json() - can be matched with any formatting when strict = false
